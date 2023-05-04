fb769721b4d57b2dffcda0ae0c9563ea3cadd9c2
Mark Suder <mark.d.suder@nasa.gov>
2023-05-04 08:44:29 -0400

    https://github.com/nasa/nos3/issues/101 - Only output torquer commands on change.

--- fsw/src/generic_adcs_output.c
+++ fsw/src/generic_adcs_output.c
@@ -14,14 +14,19 @@ void Generic_ADCS_output_to_actuators(void)
     send_mtb_commands();
 }
 
+static struct{uint8 Direction; uint8 PercentOn;} CurrentMtb[] = {{1,0}, {1,0}, {1,0}}; // Keep current state so commands are only sent on change
 static void send_mtb_commands(void)
 {
     CFE_SB_SetCmdCode((CFE_SB_Msg_t *)&Generic_ADCS_AppData.MtbPctOnCmd, 5);
     mcmd_to_percent_direction(Generic_ADCS_AppData.GNCPacket.Payload.Mcmd[0], &Generic_ADCS_AppData.MtbPctOnCmd.PercentOn_0, &Generic_ADCS_AppData.MtbPctOnCmd.Direction_0);
     mcmd_to_percent_direction(Generic_ADCS_AppData.GNCPacket.Payload.Mcmd[1], &Generic_ADCS_AppData.MtbPctOnCmd.PercentOn_1, &Generic_ADCS_AppData.MtbPctOnCmd.Direction_1);
     mcmd_to_percent_direction(Generic_ADCS_AppData.GNCPacket.Payload.Mcmd[2], &Generic_ADCS_AppData.MtbPctOnCmd.PercentOn_2, &Generic_ADCS_AppData.MtbPctOnCmd.Direction_2);
-    CFE_SB_TimeStampMsg((CFE_SB_Msg_t *)&Generic_ADCS_AppData.MtbPctOnCmd);
-    CFE_SB_SendMsg((CFE_SB_Msg_t *)&Generic_ADCS_AppData.MtbPctOnCmd);
+    if ((Generic_ADCS_AppData.MtbPctOnCmd.Direction_0 != CurrentMtb[0].Direction) || (Generic_ADCS_AppData.MtbPctOnCmd.PercentOn_0 != CurrentMtb[0].PercentOn) || 
+        (Generic_ADCS_AppData.MtbPctOnCmd.Direction_1 != CurrentMtb[1].Direction) || (Generic_ADCS_AppData.MtbPctOnCmd.PercentOn_1 != CurrentMtb[1].PercentOn) || 
+        (Generic_ADCS_AppData.MtbPctOnCmd.Direction_2 != CurrentMtb[2].Direction) || (Generic_ADCS_AppData.MtbPctOnCmd.PercentOn_2 != CurrentMtb[2].PercentOn)) {
+        CFE_SB_TimeStampMsg((CFE_SB_Msg_t *)&Generic_ADCS_AppData.MtbPctOnCmd);
+        CFE_SB_SendMsg((CFE_SB_Msg_t *)&Generic_ADCS_AppData.MtbPctOnCmd);
+    }
 }
 
 static void mcmd_to_percent_direction(double Mcmd, uint8 *percent, uint8 *direction)
