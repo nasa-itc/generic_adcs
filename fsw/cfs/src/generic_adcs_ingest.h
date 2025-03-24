3c0d28503c0e77bca119756b3be1bcc2e0eea35c
dacarter22 <98733749+dacarter22@users.noreply.github.com>
2025-03-24 18:55:22 +0000

    style: auto-format via clang-format

--- fsw/cfs/src/generic_adcs_ingest.h
+++ fsw/cfs/src/generic_adcs_ingest.h
@@ -1,6 +1,7 @@
 /*******************************************************************************
 ** Purpose:
-**   This file has the functions to ingest messages from the sensor applications.
+**   This file has the functions to ingest messages from the sensor
+*applications.
 **
 *******************************************************************************/
 #ifndef _GENERIC_ADCS_INGEST_H_
@@ -10,11 +11,17 @@
 #include "generic_adcs_msg.h"
 
 void Generic_ADCS_ingest_init(FILE *in, Generic_ADCS_DI_Tlm_Payload_t *DI);
-void Generic_ADCS_ingest_generic_mag(CFE_MSG_Message_t * Msg, Generic_ADCS_DI_Mag_Tlm_Payload_t *Mag);
-void Generic_ADCS_ingest_generic_fss(CFE_MSG_Message_t * Msg, Generic_ADCS_DI_Fss_Tlm_Payload_t *Fss);
-void Generic_ADCS_ingest_generic_css(CFE_MSG_Message_t * Msg, Generic_ADCS_DI_Css_Tlm_Payload_t *Css);
-void Generic_ADCS_ingest_generic_imu(CFE_MSG_Message_t * Msg, Generic_ADCS_DI_Imu_Tlm_Payload_t *Imu);
-void Generic_ADCS_ingest_generic_rw(CFE_MSG_Message_t * Msg, Generic_ADCS_DI_Rw_Tlm_Payload_t *Rw);
-void Generic_ADCS_ingest_generic_st(CFE_MSG_Message_t * Msg, Generic_ADCS_DI_St_Tlm_Payload_t *St);
+void Generic_ADCS_ingest_generic_mag(CFE_MSG_Message_t *Msg,
+                                     Generic_ADCS_DI_Mag_Tlm_Payload_t *Mag);
+void Generic_ADCS_ingest_generic_fss(CFE_MSG_Message_t *Msg,
+                                     Generic_ADCS_DI_Fss_Tlm_Payload_t *Fss);
+void Generic_ADCS_ingest_generic_css(CFE_MSG_Message_t *Msg,
+                                     Generic_ADCS_DI_Css_Tlm_Payload_t *Css);
+void Generic_ADCS_ingest_generic_imu(CFE_MSG_Message_t *Msg,
+                                     Generic_ADCS_DI_Imu_Tlm_Payload_t *Imu);
+void Generic_ADCS_ingest_generic_rw(CFE_MSG_Message_t *Msg,
+                                    Generic_ADCS_DI_Rw_Tlm_Payload_t *Rw);
+void Generic_ADCS_ingest_generic_st(CFE_MSG_Message_t *Msg,
+                                    Generic_ADCS_DI_St_Tlm_Payload_t *St);
 
 #endif
