b70e97924a51f640947b3f55d2ed86573038e50e
Mattilyn Walker <mattilynwalker@gmail.com>
2026-07-10 10:53:32 -0400

    [nasa/nos3#867] updating files names since find inserted _DEBUG in packets not desired

--- gsw/GENERIC_ADCS/procedures/tests/generic_adcs_app_test.py
+++ gsw/GENERIC_ADCS/procedures/tests/generic_adcs_app_test.py
@@ -31,39 +31,39 @@ def run_generic_adcs_app_test():
     ## NOOP, no operation but confirm correct counters increment
     ##
     for n in range(GENERIC_ADCS_TEST_LOOP_COUNT):
-        adcs_cmd("GENERIC_ADCS GENERIC_ADCS_NOOP_CC")
+        adcs_cmd("GENERIC_ADCS_DEBUG GENERIC_ADCS_NOOP_CC")
 
     ##
     ## Reset counters, increment as done in NOOP and confirm ability to clear repeatably
     ##
     for n in range(GENERIC_ADCS_TEST_LOOP_COUNT):
-        adcs_cmd("GENERIC_ADCS GENERIC_ADCS_NOOP_CC")
-        cmd("GENERIC_ADCS GENERIC_ADCS_RST_COUNTERS_CC") # Note standard `cmd` as we can't reset counters and then confirm increment
+        adcs_cmd("GENERIC_ADCS_DEBUG GENERIC_ADCS_NOOP_CC")
+        cmd("GENERIC_ADCS_DEBUG GENERIC_ADCS_RST_COUNTERS_CC") # Note standard `cmd` as we can't reset counters and then confirm increment
         get_adcs_hk()
-        check("GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_COUNT == 0")
-        check("GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_ERR_COUNT == 0")
+        check("GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_COUNT == 0")
+        check("GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_ERR_COUNT == 0")
 
     ##
     ##   Invalid ground command, confirm bad lengths and codes are rejected
     ##
     for n in range(GENERIC_ADCS_TEST_LOOP_COUNT):
         # Bad length
-        cmd_cnt = tlm("GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_COUNT")
-        cmd_err_cnt = tlm("GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_ERR_COUNT")
-        cmd(f"GENERIC_ADCS GENERIC_ADCS_NOOP_CC with CCSDS_LENGTH {n+2}") # Note +2 due to CCSDS already being +1
+        cmd_cnt = tlm("GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_COUNT")
+        cmd_err_cnt = tlm("GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_ERR_COUNT")
+        cmd(f"GENERIC_ADCS_DEBUG GENERIC_ADCS_NOOP_CC with CCSDS_LENGTH {n+2}") # Note +2 due to CCSDS already being +1
         get_adcs_hk()
-        # check(f"GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_COUNT == {cmd_cnt}")
-        # check(f"GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_ERR_COUNT == {cmd_err_cnt+1}")
+        # check(f"GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_COUNT == {cmd_cnt}")
+        # check(f"GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_ERR_COUNT == {cmd_err_cnt+1}")
 
     # Equivalent to Ruby's for n in 6..(5 + GENERIC_ADCS_TEST_LOOP_COUNT)
     for n in range(6, 6 + GENERIC_ADCS_TEST_LOOP_COUNT):
         # Bad command codes
-        cmd_cnt = tlm("GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_COUNT")
-        cmd_err_cnt = tlm("GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_ERR_COUNT")
-        cmd(f"GENERIC_ADCS GENERIC_ADCS_NOOP_CC with CCSDS_FC {n+1}")
+        cmd_cnt = tlm("GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_COUNT")
+        cmd_err_cnt = tlm("GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_ERR_COUNT")
+        cmd(f"GENERIC_ADCS_DEBUG GENERIC_ADCS_NOOP_CC with CCSDS_FC {n+1}")
         get_adcs_hk()
-        # check(f"GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_COUNT == {cmd_cnt}")
-        # check(f"GENERIC_ADCS GENERIC_ADCS_HK_TLM CMD_ERR_COUNT == {cmd_err_cnt+1}")
+        # check(f"GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_COUNT == {cmd_cnt}")
+        # check(f"GENERIC_ADCS_DEBUG GENERIC_ADCS_HK_TLM CMD_ERR_COUNT == {cmd_err_cnt+1}")
 
     for n in range(GENERIC_ADCS_TEST_LOOP_COUNT):
         #test data makes sense against truth
